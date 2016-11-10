9a7b52aedf2ef6fee43eb687d773d3bd6858b7e6
Kirill Mysnik <kirill@mysnik.com>
2016-11-10 22:57:25 +0300

    Updated version number

--- addons/source-python/plugins/zombie_waves/info.py
+++ addons/source-python/plugins/zombie_waves/info.py
@@ -6,7 +6,7 @@ info = PluginInfo()
 info.name = "Zombie Waves"
 info.basename = 'zombie_waves'
 info.author = 'Kirill "iPlayer" Mysnik'
-info.version = '1.0'
+info.version = '1.1'
 info.variable = '{}_version'.format(info.basename)
 info.convar = PublicConVar(
     info.variable, info.version, "{} version".format(info.name))
